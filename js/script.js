a33fabfcd6db1ba5828ce13d6cdff86517017485
Viktoriia <viktoriia.demianenko@gmail.com>
2018-04-27 11:25:30 +0300

    popup

--- js/script.js
+++ js/script.js
@@ -276,10 +276,8 @@ $(function () {
     }
 
 
-
-
     jQuery('#tel').mask('380000000000');
-    $.validator.addMethod("letters", function(value, element) {
+    $.validator.addMethod("letters", function (value, element) {
         return this.optional(element) || value == value.match(/^[\-a-zA-ZА-ЯЁа-яё\s]*$/);
     });
 
@@ -335,14 +333,11 @@ $(function () {
                     swal({
                         text: 'Спасибо! Мы уже обрабатываем Вашу заявку и в ближайшее время свяжемся с Вами.',
                         type: 'success',
-                        confirmButtonText: 'Отлично',
-                        buttonsStyling: 'false',
-                        confirmButtonClass: 'btn'
+                        confirmButtonText: 'Отлично'
                     });
-
-                         $('#fName').val('');
-                         $('#email').val('');
-                         $('#tel').val('');
+                    $('#fName').val('');
+                    $('#email').val('');
+                    $('#tel').val('');
                 },
                 error: function (data) {
                     swal({
@@ -354,10 +349,4 @@ $(function () {
             });
         }
     });
-
-
-
-
-
-
 });
